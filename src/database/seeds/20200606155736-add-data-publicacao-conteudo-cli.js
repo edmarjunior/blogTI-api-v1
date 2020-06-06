859f40d26ee7a5d74a783ef7131b4936f0bf32c3
Edmar Costa <edmar.acjr@gmail.com>
2020-06-06 16:10:00 -0300

    Alteração na seed de data de publicação do conteudo CLI para inserir com timezone

--- src/database/seeds/20200606155736-add-data-publicacao-conteudo-cli.js
+++ src/database/seeds/20200606155736-add-data-publicacao-conteudo-cli.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   up: queryInterface => {
-    return queryInterface.sequelize.query("UPDATE conteudos SET data_publicacao='2020-05-24' WHERE id=1");
+    return queryInterface.sequelize.query("UPDATE conteudos SET data_publicacao='2020-05-24T03:00:00.000Z' WHERE id=1");
   },
 
   down: queryInterface => {
